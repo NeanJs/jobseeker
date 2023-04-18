4eb33d3f7e1e6e9e34e30f4dc49d2dac9d12a32d
neanjs <neancodes@gmail.com>
2023-04-18 07:43:22 +0545

    deploy workflow implemented

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, preview } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/jobseeker/"
+  base:"/jobseeker/",
+  preview:{
+    port:8080,
+  },
+  server:{
+    port:3000,
+  }
 })
