d2fc381fa19d3d1080f647c8bf655e22c556779d
Nijan Adhikari <72246077+NeanJs@users.noreply.github.com>
2025-02-09 00:44:05 -0800

    Update axiosInstance.tsx

--- client/src/services/axiosInstance.tsx
+++ client/src/services/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   // @ts-ignore
-  baseURL: process.env.BASER_URL,
+  baseURL: import.meta.env.VITE_BASER_URL,
   timeout: 10000,
 });
 export default axiosInstance;
